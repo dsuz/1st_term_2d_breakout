b839fd3b0eb22ff5ce083259130dc8c0f0b480ef
bboydaisuke <bboydaisuke@gmail.com>
2018-05-06 11:55:52 +0900

    アセット追加、ボールとパドルのスクリプト追加、ボール・パドル・枠が配置されたシーンの追加

---
+++ Assets/BallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] Vector2 m_power;
+    [SerializeField] float m_powerScale;
+    Rigidbody2D m_rb2d;
+
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+        m_rb2d.AddForce(m_power * m_powerScale);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/PaddleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    [SerializeField] float m_speed = 1.0f;
+    Rigidbody2D m_rb2d;
+
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        m_rb2d.AddForce(h * Vector2.right * m_speed);
+    }
+}
