d8b8ab77341644c8aa42c78f15eab6bf6a9381e2
bboydaisuke <bboydaisuke@gmail.com>
2018-05-06 21:38:06 +0900

    ターゲットとなるブロック及びブロックを生成するスクリプトを追加した

---
+++ Assets/TargetBlockController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBlockController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/TargetBlockGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBlockGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject[] m_targetPrefabArray;
+    [SerializeField] Vector2 m_startGeneratePostion;
+    [SerializeField] int m_columns = 7;
+    [SerializeField] int m_rows = 6;
+
+    void Start()
+    {
+        Vector2 pos = m_startGeneratePostion;
+        float blockHeight = 0f, blockWidth = 0f;
+        for (int j = 0; j < m_rows; j++)
+        {
+            for (int i = 0; i < m_columns; i++)
+            {
+                int prefabIndex = j % m_targetPrefabArray.Length;
+                GameObject go = Instantiate(m_targetPrefabArray[prefabIndex], this.gameObject.transform);
+                go.transform.position = pos;
+                blockWidth = go.GetComponent<SpriteRenderer>().bounds.size.x;
+                blockHeight = go.GetComponent<SpriteRenderer>().bounds.size.y;
+                pos = pos + new Vector2(blockWidth, 0);
+            }
+            pos = new Vector2(m_startGeneratePostion.x, pos.y - blockHeight);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
