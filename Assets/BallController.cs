11af77c78ef4f91bdf0c34f8128db41ec2d26689
bboydaisuke <bboydaisuke@gmail.com>
2018-05-17 23:46:55 +0900

    ゲームオーバーにする機能を追加した

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -13,20 +13,6 @@ public class BallController : MonoBehaviour
     [SerializeField] float m_powerScale;
     Rigidbody2D m_rb2d;
 
-    void Start()
-    {
-        m_rb2d = GetComponent<Rigidbody2D>();
-        m_rb2d.AddForce(m_power * m_powerScale);
-        GameObject go = GameObject.Find("ScoreText"); // ScoreText という名前のオブジェクトを探してとっておく
-        m_scoreText = go.GetComponent<Text>();  // ScoreText という名前のオブジェクトから Text を取り出す
-        m_scoreText.text = m_score.ToString();  // ToString() を使って整数を文字列に変換する
-    }
-
-    void Update()
-    {
-
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         TargetBlockController target = collision.gameObject.GetComponent<TargetBlockController>();  // 衝突相手がら「ぶつかったら壊れるブロック」の取得を試みる
@@ -35,5 +21,17 @@ public class BallController : MonoBehaviour
             m_score += target.m_score;
             m_scoreText.text = m_score.ToString();
         }
+    }
+
+    /// <summary>元々 Start() にあった内容はここに移動する。</summary>
+    public void Reset()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();   // RigidBody を取得する
+        m_rb2d.velocity = Vector2.zero;     // 一旦速度をリセットする。ゲームオーバーから再開した時のため
+        m_rb2d.AddForce(m_power * m_powerScale);    // 力を加えてボールを動かす
+        GameObject go = GameObject.Find("ScoreText"); // ScoreText という名前のオブジェクトを探してとっておく
+        m_scoreText = go.GetComponent<Text>();  // ScoreText という名前のオブジェクトから Text を取り出す
+        m_scoreText.text = m_score.ToString();  // ToString() を使って整数を文字列に変換する
+        m_score = 0;    // スコアをリセットする
     }
 }
---
+++ Assets/GameController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>ゲーム全体を制御する</summary>
+public class GameController : MonoBehaviour
+{
+    /// <summary>ゲーム中かどうかを判定するフラグ</summary>
+    bool m_isInGame = false;
+    /// <summary>ボール</summary>
+    BallController m_ball;
+    /// <summary>ボールの初期位置</summary>
+    Vector2 m_initialPositionOfBall;
+    /// <summary>メッセージを表示する Text</summary>
+    Text m_console;
+    /// <summary>ボールの y 座標がこれより小さくなったらゲームオーバー</summary>
+    float m_bottomBorder = -10f;
+
+	void Start ()
+    {
+        InitializeGame();
+    }
+	
+	void Update ()
+    {
+		if (m_isInGame) // 今ゲーム中の場合は
+        {
+            if (m_ball.transform.position.y < m_bottomBorder)   // ボールの場所を確認してあまりにも下だったらゲームオーバー
+            {
+                EndGame();
+            }
+        }
+        else
+        {
+            if (Input.GetKeyUp(KeyCode.Return)) // ゲーム中でない時に Enter が押されたらゲームを開始する
+            {
+                StartGame();
+            }
+        }
+	}
+
+    /// <summary>初期化する</summary>
+    void InitializeGame()
+    {
+        GameObject ballObject = GameObject.Find("red_ball");    // ボールのオブジェクトを取得する
+        m_initialPositionOfBall = ballObject.transform.position;    // ボールの初期位置を保存しておく
+        m_ball = ballObject.GetComponent<BallController>();     // ボールから BallController を取り出しておく
+        GameObject consoleObject = GameObject.Find("ConsoleText");  // メッセージを表示する Text を取得する
+        m_console = consoleObject.GetComponent<Text>();
+        m_console.text = "Hit Enter To Start";  // メッセージを設定する
+    }
+
+    /// <summary>ゲームを開始する</summary>
+    void StartGame()
+    {
+        m_ball.transform.position = m_initialPositionOfBall;    // ボールを初期位置に移動する
+        m_ball.Reset(); // ゲーム開始
+        m_isInGame = true;  // フラグを立てる
+        m_console.text = "";    // メッセージを消す
+    }
+
+    /// <summary>ゲームオーバーにする</summary>
+    void EndGame()
+    {
+        m_isInGame = false; // フラグをゲーム中ではない状態にする
+        m_console.text = "Game Over\r\nHit Enter To Restart";   // メッセージを表示する
+    }
+}
